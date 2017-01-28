010b310c5f26709e150c3b3a19eaf67f8fce696b
Nygel Lopez <nygel.lopez@mail.utoronto.ca>
2017-01-27 22:33:13 -0500

    Refactoring

--- index.php
+++ index.php
@@ -12,8 +12,7 @@
 	/* controller code */
 	if(!isset($_SESSION['state'])){
 		$_SESSION['state']='login';
-		$_SESSION['iGetIt']=new iGetIt();
-		$_SESSION['dbconn']=$dbconn;
+		$_SESSION['iGetIt']=new iGetIt($dbconn);
 	}
 
 	switch($_SESSION['state']){
@@ -36,7 +35,7 @@
 			if(!empty($errors))break;
 
 			// checks user login, and if exists, then go to landing page
-			if($row = $_SESSION['iGetIt']->validateLogin($dbconn,$_REQUEST['user'], $_REQUEST['password'])){
+			if($row = $_SESSION['iGetIt']->validateLogin($_REQUEST['user'], $_REQUEST['password'])){
 			    if($row["type"]=="instructor"){
                     $_SESSION['state']='instructor_create';
                     $view="instructor_createclass.php";
@@ -46,7 +45,7 @@
                 }
                 // if does not exist, then go to profile
 			} else {
-				if($row = $_SESSION['iGetIt']->validateUser($dbconn,$_REQUEST['user'])){
+				if($row = $_SESSION['iGetIt']->validateUser($_REQUEST['user'])){
 					$errors[]='invalid login';
 				} else {
 					$_SESSION['state']='profile';
@@ -79,12 +78,12 @@
               }
 
               // check if username taken
-            if($row = $_SESSION['iGetIt']->validateUser($dbconn,$_REQUEST['user'])){
+            if($row = $_SESSION['iGetIt']->validateUser($_REQUEST['user'])){
                   $errors[]='user already exists';
 
               // Otherwise, create the user and move to selected landing page (i.e. instructor or student)
             } else {
-                $_SESSION['iGetIt']->createUser($dbconn,$_REQUEST['user'],$_REQUEST['password'],$_REQUEST['firstName'],
+                $_SESSION['iGetIt']->createUser($_REQUEST['user'],$_REQUEST['password'],$_REQUEST['firstName'],
                     $_REQUEST['lastName'],$_REQUEST['email'],$_REQUEST['type']);
                 if($_REQUEST['type']=="instructor"){
                     $_SESSION['state']='instructor_create';
--- model/iGetIt.php
+++ model/iGetIt.php
@@ -2,28 +2,28 @@
 
 class iGetIt {
 
-
-	public function __construct() {
-
+    public $dbconn;
+	public function __construct($dbconn) {
+        $this->dbconn=$dbconn;
     }
 
-	public function validateLogin($dbconn, $user, $password){
-        $result = pg_prepare($dbconn, "loginQuery", "SELECT * FROM appuser where username=$1 and password=$2");
-        $result = pg_execute($dbconn, "loginQuery", array($user, $password));
+	public function validateLogin($user, $password){
+        $result = pg_prepare($this->dbconn, "loginQuery", "SELECT * FROM appuser where username=$1 and password=$2");
+        $result = pg_execute($this->dbconn, "loginQuery", array($user, $password));
         return pg_fetch_array($result);
 	}
-    public function validateUser($dbconn, $user){
-        $result = pg_prepare($dbconn, "userQuery", "SELECT * FROM appuser where username=$1");
-        $result = pg_execute($dbconn, "userQuery", array($user));
+    public function validateUser($user){
+        $result = pg_prepare($this->dbconn, "userQuery", "SELECT * FROM appuser where username=$1");
+        $result = pg_execute($this->dbconn, "userQuery", array($user));
         return pg_fetch_array($result);
     }
-    public function createUser($dbconn, $user, $password, $fName, $lName, $email, $type){
-        $result = pg_prepare($dbconn, "insertUser", "INSERT INTO appuser values($1,$2,$3,$4,$5,$6)");
-        $result = pg_execute($dbconn, "insertUser", array($user, $password, $fName, $lName, $email, $type));
+    public function createUser($user, $password, $fName, $lName, $email, $type){
+        $result = pg_prepare($this->dbconn, "insertUser", "INSERT INTO appuser values($1,$2,$3,$4,$5,$6)");
+        $result = pg_execute($this->dbconn, "insertUser", array($user, $password, $fName, $lName, $email, $type));
     }
-    public function getAvailableClasses($dbconn){
-        $result = pg_prepare($dbconn, "getClasses", "SELECT * FROM classes");
-        $result = pg_execute($dbconn, "getClasses", array());
+    public function getAvailableClasses(){
+        $result = pg_prepare($this->dbconn, "getClasses", "SELECT * FROM classes");
+        $result = pg_execute($this->dbconn, "getClasses", array());
         return $result;
     }
     public function createClass(){}
