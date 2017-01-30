b3fd278787dca20a6f7c871a03df7d6a960a30e9
Nygel Lopez <nygel.lopez@mail.utoronto.ca>
2017-01-30 17:45:41 -0500

    Setup script

--- dbconn.php
+++ dbconn.php
@@ -1,4 +1,4 @@
 <?php
 // modify credentials to the database connection here
-$dbconn = pg_connect("host=mcsdb.utm.utoronto.ca dbname=lopeznyg_309 user=lopeznyg password=13779");
+$dbconn = pg_connect("host=mcsdb.utm.utoronto.ca dbname=dbnamehere user=usernamehere password=passwordhere");
 ?>
