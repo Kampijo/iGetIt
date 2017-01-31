3ebc4f1180624f073ec1b9b827605f6336f17dcd
Nygel Lopez <nygel.lopez@mail.utoronto.ca>
2017-01-31 17:29:08 -0500

    revised setup

--- setup.bash
+++ setup.bash
@@ -2,6 +2,7 @@ read -p "Enter host: " host
 read -p "Enter database name: " dbname
 read -p "Enter UTORid: " utorid
 read -s -p "Enter password: " password
+echo -e "\n"
 
 sed -i "s/hosthere/$host/g" dbconn.php
 sed -i "s/dbnamehere/$dbname/g" dbconn.php
